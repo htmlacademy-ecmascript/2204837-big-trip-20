0dd5ce5d255c16e1eede0707d9b67dec76635d5f
corpp <corppwork@gmail.com>
2023-05-02 16:14:24 +0300

    Домашнее задание 1.18, корректировка расположения компонента фильтрации

--- src/presenter/header-info-presener.js
+++ src/presenter/header-info-presener.js
@@ -1,14 +1,13 @@
 import HeaderInfoView from '../view/header-info';
-import FilterView from '../view/filter-view';
 import { render, RenderPosition } from '../render';
 
-export default class HeaderPresenter {
+export default class HeaderInfoPresenter {
   constructor ({headContainer}) {
     this.headContainer = headContainer;
   }
 
   init () {
     render (new HeaderInfoView, this.headContainer, RenderPosition.AFTERBEGIN);
-    render (new FilterView, this.headContainer);
   }
 }
+
